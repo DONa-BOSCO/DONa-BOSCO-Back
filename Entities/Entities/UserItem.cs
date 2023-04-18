96b5a5f030001edf65e9cd0f10fb2dd669eaae1d
AldaraMG~ <almaga71@gmail.com>
2023-04-18 12:15:57 +0200

    atributes user

--- Entities/Entities/UserItem.cs
+++ Entities/Entities/UserItem.cs
@@ -4,7 +4,12 @@
     {
         public int Id { get; set; }
         public int IdRol { get; set; }
-        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+
+        public string Password { get; set; }
         public DateTime InsertDate { get; set; }
         public DateTime UpdateDate { get; set; }
         public bool IsActive { get; set; }
