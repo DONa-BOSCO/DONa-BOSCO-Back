6a2a9db2b8df1e22d78dd23d850f31526082a0ec
MagdalenaRB <magdalenareig@gmail.com>
2023-04-18 14:03:09 +0200

    begin user register

--- Resources/RequestModels/NewUserRequest.cs
+++ Resources/RequestModels/NewUserRequest.cs
@@ -7,13 +7,20 @@ namespace Resources.RequestModels
         public int IdRol { get; set; }
         public string UserName { get; set; }
         public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
 
         public UserItem ToUserItem()
         {
             var userItem = new UserItem();
 
             userItem.IdRol = 2;
+            userItem.FirstName = FirstName;
+            userItem.LastName = LastName;
             userItem.UserName = UserName;
+            userItem.Email = Email;
+            userItem.Password = Password;
             userItem.InsertDate = DateTime.Now;
             userItem.UpdateDate = DateTime.Now;
             userItem.IsActive = true;
